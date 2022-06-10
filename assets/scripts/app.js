01fc2053adefcdb56aa21705c1f9df3d44573d67
ssesto1 <sestosinisa@gmail.com>
2022-06-10 17:07:07 +0200

    added return value into function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,10 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
     const result = num1 + num2;
-    alert('The result is ' + result);
+    return result;
 }
 
-add(1,2);
-add(5,6);
-
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+currentResult = add(1,2);
 let calculationDesscription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDesscription);
